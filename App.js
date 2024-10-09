b78a3f94e6ee2a8885b89803405c9a4239c452e5
Aatu Huttula <91623487+AatuTHu@users.noreply.github.com>
2024-10-09 11:33:09 +0300

    Started on navigation
    Tried to do navigation function where user can go back to last page by pressing back button.

--- App.js
+++ App.js
@@ -1,10 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Button, BackHandler } from 'react-native';
+import { useState, useEffect } from 'react'
+import Page1 from './components/Page1'
+import Page2 from './components/Page2'
+import Page3 from './components/Page3'
 
 export default function App() {
+
+  const [page, setPage] = useState(1)
+  const [prevPage, setPrevPage] = useState(1)
+
+  useEffect(() => {
+    if (prevPage !== page) {
+      setPrevPage(page);
+    }
+  }, [page])
+
+  useEffect(() => {
+    // Function to handle the back button press
+    const backAction = () => {
+      if(page == 1) {
+        BackHandler.exitApp()
+      } else if (page === prevPage) {
+        setPage(1)
+        return true; // Return `true` to prevent default back action
+      } else {
+        setPage(prevPage)
+        return true; // Return `true` to prevent default back action
+      }
+
+    };
+
+    // Add event listener for back press
+    const backHandler = BackHandler.addEventListener(
+      'hardwareBackPress',
+      backAction
+    );
+    return () => backHandler.remove();
+  }, [page, prevPage]);
+
+  
+
+  const navigator = () => {
+    // Implement navigation logic here
+    // For example, navigate to a new screen or route
+   switch (page) {
+    case 1:
+      return <Page1/>
+    case 2:
+      return <Page2/>
+    case 3:
+      return <Page3/>
+    default:
+      return <Page1 />;
+   }
+  }
+ 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {navigator()}
+      <Button title="Page 1" onPress={() => setPage(1)} />
+      <Button title="Page 2" onPress={() => setPage(2)} />
+      <Button title="Page 3" onPress={() => setPage(3)} />
       <StatusBar style="auto" />
     </View>
   );
